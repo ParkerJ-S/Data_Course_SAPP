81466848b053740228762022afbc66ad9b000bd2
Sapp <sappparkerj@gmail.com>
2025-09-25 19:28:02 -0600

    Assignment_2.R

---
+++ Assignment_2.R
@@ -0,0 +1,70 @@
+read.csv(file = 'Data/BioLog_Plate_Data.csv')
+
+dat_bioplate <- read.csv(file = 'Data/Biolog_Plate_Data.csv')
+
+read.csv(file = 'Data/BioLog_Plate_Data.csv')
+
+list.files('Data/BioLog_Plate_Data.csv')
+
+## loop
+
+I like cherries
+I like raspberries
+I like apple
+
+read.csv(file = 'Data/BioLog_Plate_Data.csv')
+vec_fruit = c('cherries', 'raspberries', 'apple')
+for (variable in vec_fruit) { print(variable)
+  
+}
+for (i in vec_fruit) {
+  print(i)
+  print(i+1)
+}
+
+read.csv(files)
+read.csv(file)
+read.csv(file = 'Data')
+list.files(path = Desktop/School/Data_Analytics/Data_Course_SAPP/Data)
+list.files()
+list.files(path = 'Data')
+csv_file <- list.files(path = 'Data', pattern = "\\.csv$", full.names = FALSE)
+
+x = 1+2
+length(list.files(path = "Data", pattern = "\\.csv$", ignore.case = TRUE))
+list.files('wingspan_vs_mass.csv')
+read.csv('wingspan_vs_mass.csv')
+read.csv(file = 'wingspan_vs_mass.csv')
+list.files(csv_file)
+read.csv('csv_file')
+read.csv(file = "wingspan_vs_mass.csv")
+read.csv('wingspan_vs_mass.csv')
+read.csv('Data/BioLog_Plate_Data.csv')
+list.files('Data/BioLog_Plate_Data.csv')
+getwd()
+read.csv('Data/BioLog_Plate_Data.csv')
+dat = read.csv('Data/BioLog_Plate_Data.csv')
+dat
+csv_files = read.csv('Data/BioLog_Plate_Data.csv')
+csv_files
+csv_files = list.files(path = "Data", pattern = "\\.csv$", ignore.case = TRUE)
+csv_files
+View(list.files('Data/wingspan_vs_mass.csv'))
+read.csv('Data/wingspan_vs_mass.csv')
+df = read.csv('Data/wingspan_vs_mass.csv')
+View(df)
+View(df[1, 2, 3, 4, 5])
+View(head(df[1, 2, 3, 4, 5]))
+getwd
+getwd()
+df
+head(df, n= 5)
+b_file = list.files('Data/', pattern = "^b", recursive = T)
+b_file
+View('Data/')
+list.files('Data/', pattern = "^b", recursive = T)
+file_path = "Data"
+all_files = list.files('Data/', pattern = '^b', recursive = T)
+print(all_files)
+csv_files = list.files('Data/', pattern = "\\.csv$", full.names = T)
+csv_files
