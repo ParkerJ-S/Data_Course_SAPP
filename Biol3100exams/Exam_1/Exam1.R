31b35377f8f775f727e46ce7782161385b2770a0
Sapp <sappparkerj@gmail.com>
2025-12-12 01:16:31 -0700

    Exam1.R

--- Biol3100exams/Exam_1/Exam1.R
+++ Biol3100exams/Exam_1/Exam1.R
@@ -40,32 +40,33 @@ The files you just got from your "git pull" are:
   
   A glimpse of the data structure:
   ```{r echo=FALSE, message=FALSE, warning=FALSE}
+
 library(tidyverse)
-df <- read_csv("cleaned_covid_data.csv")
+df = read_csv("./Biol3100exams/Exam_1/cleaned_covid_data.csv")
 skimr::skim(df)
 ```
 
 # YOUR TASKS:
 
-**I.**
-  **Read the cleaned_covid_data.csv file into an R data frame. (20 pts)**
+#I.**
+#  **Read the cleaned_covid_data.csv file into an R data frame. (20 pts)**
   
-read_csv('Biol3100exams/BIOL3100_Exams/Exam_1/cleaned_covid_data.csv')
+read_csv('Biol3100exams/Exam_1/cleaned_covid_data.csv')
   
-  **II.**
-  **Subset the data set to just show states that begin with "A" and save this as
-an object called A_states. (20 pts)**
+#  **II.**
+#  **Subset the data set to just show states that begin with "A" and save this as
+#an object called A_states. (20 pts)**
 
 view(df)  
-df = read_csv('Biol3100exams/BIOL3100_Exams/Exam_1/cleaned_covid_data.csv')
+df = read_csv('Biol3100exams/Exam_1/cleaned_covid_data.csv')
 A_states = df[grepl("^A", df$Province_State), ]
      
-  + Use the *tidyverse* suite of packages
-+ Selecting rows where the state starts with "A" is tricky (you can use the grepl() function
-or just a vector of those states if you prefer)
+#  + Use the *tidyverse* suite of packages
+#+ Selecting rows where the state starts with "A" is tricky (you can use the grepl() function
+#or just a vector of those states if you prefer)
 
-**III.**
-  **Create a plot _of that subset_ showing Deaths over time, with a separate facet for each state. (20 pts)**
+#**III.**
+#  **Create a plot _of that subset_ showing Deaths over time, with a separate facet for each state. (20 pts)**
   
 D_over_T =ggplot(subset.data.frame(A_states), aes(x = Last_Update, y = Deaths))+
   geom_point(color = "red") +
@@ -73,13 +74,13 @@ D_over_T =ggplot(subset.data.frame(A_states), aes(x = Last_Update, y = Deaths))+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   facet_wrap(~Province_State, scales = "free")
 
-  + Create a scatterplot
-+ Add loess curves WITHOUT standard error shading
-+ Keep scales "free" in each facet
+#  + Create a scatterplot
+#+ Add loess curves WITHOUT standard error shading
+#+ Keep scales "free" in each facet
 
-**IV.** (Back to the full dataset)
-**Find the "peak" of Case_Fatality_Ratio for each state and save this as a new data frame object 
-called state_max_fatality_rate. (20 pts)**
+#**IV.** (Back to the full dataset)
+#**Find the "peak" of Case_Fatality_Ratio for each state and save this as a new data frame object 
+#called state_max_fatality_rate. (20 pts)**
   
 state_max_fatality_rate = data.frame(
     Province_State = df$Province_State,
@@ -89,32 +90,32 @@ state_max_fatality_rate_sorted = state_max_fatality_rate %>%
   arrange(desc(Maximum_Fatality_Ratio))
 view(state_max_fatality_rate_sorted)
 
-  Im looking for a new data frame with 2 columns:
- + "Province_State"
- + "Maximum_Fatality_Ratio"
- + Arrange the new data frame in descending order by Maximum_Fatality_Ratio
+#  Im looking for a new data frame with 2 columns:
+#+ "Province_State"
+# + "Maximum_Fatality_Ratio"
+# + Arrange the new data frame in descending order by Maximum_Fatality_Ratio
  
-This might take a few steps. Be careful about how you deal with missing values!
+#This might take a few steps. Be careful about how you deal with missing values!
 
 
-**V.**
-**Use that new data frame from task IV to create another plot. (20 pts)**
+#**V.**
+#**Use that new data frame from task IV to create another plot. (20 pts)**
   
 plot_2 = ggplot(state_max_fatality_rate_sorted, aes(x = Province_State, y = Maximum_Fatality_Ratio)) +
   geom_bar(stat = "identity", color = "blue")+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
- + X-axis is Province_State
- + Y-axis is Maximum_Fatality_Ratio
- + bar plot
- + x-axis arranged in descending order, just like the data frame (make it a factor to accomplish this)
- + X-axis labels turned to 90 deg to be readable
+# + X-axis is Province_State
+# + Y-axis is Maximum_Fatality_Ratio
+# + bar plot
+# + x-axis arranged in descending order, just like the data frame (make it a factor to accomplish this)
+# + X-axis labels turned to 90 deg to be readable
  
-Even with this partial data set (not current), you should be able to see that (within these dates), 
-different states had very different fatality ratios.
+#Even with this partial data set (not current), you should be able to see that (within these dates), 
+#different states had very different fatality ratios.
 
-**VI.** (BONUS 10 pts)
-**Using the FULL data set, plot cumulative deaths for the entire US over time**
+#**VI.** (BONUS 10 pts)
+#**Using the FULL data set, plot cumulative deaths for the entire US over time**
 
- + Youll need to read ahead a bit and use the dplyr package functions group_by() and summarize() to accomplish this.
+# + Youll need to read ahead a bit and use the dplyr package functions group_by() and summarize() to accomplish this.
 
